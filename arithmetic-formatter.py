f88ce1de0cbb4820635ce1dae5b12d87b0243f38
oleksandrpopov <shas02qazxsw@gmail.com>
2025-06-19 16:43:21 +0300

    improved arithmetic-formatter.py, added password-generator.py

--- arithmetic-formatter.py
+++ arithmetic-formatter.py
@@ -1,103 +1,49 @@
 from typing import List
 
-def arithmetic_arranger(problems: List[str], show_answers: bool = False):
+def arithmetic_arranger(problems: List[str], show_answers: bool = False) -> str:
     if len(problems) > 5:
-        raise ValueError('Error: Too many problems.')
+        return 'Error: Too many problems.'
 
-    topParts: str = '\n'
-    bottomParts: str = '\n'
-    dashesParts: str = '\n'
-    results: str = ''
+    topParts: List[str] = []
+    bottomParts: List[str] = []
+    dashesParts: List[str] = []
+    results: List[str] = []
 
     for problem in problems:
-        partsOfProblem: List[str] = problem.split(' ')
+        parts: List[str] = problem.split(' ')
+        
+        if len(parts) != 3:
+            return "Error: Invalid format."
 
-        number1: str = partsOfProblem[0]
-        validateNumber(number1)
+        number1, operator, number2 = parts
 
-        number2: str = partsOfProblem[2]
-        validateNumber(number2)
+        if not number1.isdigit() or not number2.isdigit():
+            return "Error: Numbers must only contain digits."
+        if len(number1) > 4 or len(number2) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        if operator not in ["+", "-"]:
+            return "Error: Operator must be '+' or '-'."
 
-        operator: str = partsOfProblem[1]
-        validateOperator(operator)
+        width: int = max(len(number1), len(number2)) + 2
 
-        partLen: int = getPartLen(number1, number2)
-
-        topParts += formTopPart(number1, partLen)
-        bottomParts += formBottomPart(operator, number2, partLen)
-        dashesParts += formDashes(partLen)
+        topParts.append(number1.rjust(width))
+        bottomParts.append(operator + number2.rjust(width - 1))
+        dashesParts.append('-' * width)
 
         if show_answers:
-            result: int = calculateResult(number1, number2, operator)
-            results += formResultPart(result, partLen)
+            result = str(eval(number1 + operator + number2))
+            results.append(result.rjust(width))
 
-    butyfiedProblems: str = topParts + '\n' + bottomParts + '\n' + dashesParts
+    arranged_problems: str = (
+            '    '.join(topParts) + '\n' +
+            '    '.join(bottomParts) + '\n' +
+            '    '.join(dashesParts)
+    )
+
     if show_answers:
-        butyfiedProblems += '\n' + results
+        arranged_problems += '\n' + '    '.join(results)
 
-    return butyfiedProblems
-
-def validateNumber(number: str):
-    if not number.isdigit():
-        raise ValueError('Error: Numbers must only contain digits.')
-    if len(number) > 4:
-        raise ValueError('Error: Numbers cannot be more than four digits.')
-
-def validateOperator(operator: str):
-    if not operator == "+" and not operator == "-":
-        raise ValueError("Error: Operator must be '+' or '-'.")
-
-def getPartLen(number1: str, number2: str) -> int:
-    number1Len: int = len(number1)
-    number2Len: int = len(number2)
-    if number1Len > number2Len:
-        longestNumberLen = number1Len
-    else:
-        longestNumberLen = number2Len
-
-    return longestNumberLen + 2
-
-
-def formTopPart(number: str, partLen: int) -> str:
-    topPart: str = ''
-    for _ in range(partLen - len(number)):
-        topPart += " "
-    topPart += number + "    "
-    return topPart
-
-def formBottomPart(operator: str, number: str, partLen: int) -> str:
-    bottomPart: str = operator + ' '
-    for _ in range(partLen - 2 - len(number)):
-        bottomPart += " "
-    bottomPart += number + "    "
-    return bottomPart
-
-def formDashes(partLen: int) -> str:
-    dashes: str = ''
-    for _ in range(partLen):
-        dashes += "-"
-    dashes += "    "
-    return dashes
-
-def calculateResult(number1: str, number2: str, operator: str) -> int:
-    number1Int: int = int(number1)
-    number2Int: int = int(number2)
-    result: int = 0
-    if operator == "+":
-        result = number1Int + number2Int
-    elif operator == "-":
-        result = number1Int - number2Int
-    return result
-
-def formResultPart(result: int, partLen: int) -> str:
-    resultPart: str = ''
-    resultStr: str = str(result)
-    for _ in range(partLen - len(resultStr)):
-        resultPart = " " + resultPart
-
-    resultPart += resultStr + "    "
-
-    return resultPart
+    return arranged_problems
 
 
 print(f'\n{arithmetic_arranger(["3801 - 2", "123 + 49"])}')
